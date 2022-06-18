24bc4a362304ebf0a819fb18f5cb9ef3d1a8828f
Ruman Sanghera <rumansanghera@gmail.com>
2022-06-18 16:37:34 +0100

    Remote SQL Connect

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', '');
+$connection = mysqli_connect('flowtrackdb.database.windows.net', 'RumanAdmin', 'Admin12345');
 if (!$connection) {
     die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection, 'test');
+$select_db = mysqli_select_db($connection, 'FlowTrackDatabase');
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 }

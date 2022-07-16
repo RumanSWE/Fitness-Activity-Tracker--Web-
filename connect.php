66aec4d945532d51238abeeee96de4b93dbcd88f
Ruman Sanghera <rumansanghera@gmail.com>
2022-07-16 12:52:53 +0100

    Attempting to Fix CSS

--- connect.php
+++ connect.php
@@ -1,5 +1,7 @@
 <?php
-$connection = mysqli_connect('flowtrackdb.database.windows.net', 'RumanAdmin', 'Admin12345');
+$connection = mysqli_connect('flowtrackdb.database.windows.net', 'RumanAdmin', 'Admin12345','FlowTrackDatabase');
+//$connection  = new PDO("sqlsrv:server = tcp:flowtrackdb.database.windows.net,1433; Database = FlowTrackDatabase", "RumanAdmin", "Admin12345");
+    //$connection ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if (!$connection) {
     die("Database Connection Failed" . mysqli_error($connection));
 }
--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php  //Start the Session
 session_start();
- require('connect.php');
+
+ //require('connect.php');
+
 //3. If the form is submitted or not.
 //3.1 If the form is submitted
 if (isset($_POST['username']) and isset($_POST['password'])){

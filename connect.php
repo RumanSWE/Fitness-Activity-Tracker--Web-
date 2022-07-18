4c090f0e21c6806e87c788c473032aec37193982
Ruman Sanghera <rumansanghera@gmail.com>
2022-07-18 14:47:08 +0100

    Fixed connection to AWS RDS free tier

--- connect.php
+++ connect.php
@@ -1,8 +1,6 @@
 <?php
-//$connection = mysqli_connect('flowtrackdb.database.windows.net', 'RumanAdmin', 'Admin12345','FlowTrackDatabase');
-//$connection  = new PDO("sqlsrv:server = tcp:flowtrackdb.database.windows.net,1433; Database = FlowTrackDatabase", "RumanAdmin", "Admin12345");
-$connection = mysqli_connect('database-1-instance-1.ctqvev74bys7.eu-west-1.rds.amazonaws.com:3306', 'admin', 'halo47reach','test');
-    //$connection ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$connection = mysqli_connect('database-1.cy53qqkottd0.us-east-1.rds.amazonaws.com:3306', 'admin', 'halo47reach','test');
 if (!$connection) {
     die("Database Connection Failed" . mysqli_error($connection));
 }
